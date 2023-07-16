6082071a5a18b49bfb7cd4d16e5e17c4cd4c9b06
future9061 <132829711+future9061@users.noreply.github.com>
2023-07-16 12:03:07 +0900

    Delete src/content directory

--- src/content/SideContent.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from "./SideContent.module.css";
-
-export default function SideContent() {
-  return <div className={classes.side_content_wrap}>side</div>;
-}
