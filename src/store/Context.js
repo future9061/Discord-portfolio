935cc1ed359ac14bbc845481ef959ca45a310016
future9061 <132829711+future9061@users.noreply.github.com>
2023-07-20 18:15:34 +0900

    Update Context.js

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,10 +1,15 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const ThemeContext = React.createContext();
 
 const ThemeProvider = ({ children }) => {
   const LocalTheme = window.localStorage.getItem("theme") || "basic";
   const [themeMode, setThemeMode] = useState(LocalTheme);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  
 
   //  클릭하면 atCircle이 아니라 텍스트에 저장
   //텍스트의 값이 없으면 Home 있으면 클릭한 값
