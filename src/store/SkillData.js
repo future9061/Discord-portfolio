088db89c92e9e1b83d7cdd0b35ed0f77c04ab4c3
Lee Mi Rae <132829711+future9061@users.noreply.github.com>
2023-07-28 17:38:37 +0900

    Add files via upload

---
+++ src/store/SkillData.js
@@ -0,0 +1,68 @@
+const SkillData = [
+  {
+    title: "",
+    content: "클릭으로 해당 스킬의 구체적인 역량을 확인할 수 있어요!",
+  },
+  {
+    title: "Javascript",
+    content:
+      "자료형에 대한 이해를 바탕으로 dataBinding을 할 수 있으며,selector로 DOM 요소에 접근해 조작, 이벤트 핸들러 등록 후 각종 이벤트 활용이 가능합니다.",
+  },
+  {
+    title: "HTML",
+    content:
+      "Semantic Tag로 HTML tag를 적재적소에 사용하여 접근성 향상 및 검색 엔진(SEO)에 최적화 된 웹 페이지 구조를 구축할 수 있습니다.",
+  },
+  {
+    title: "CSS",
+    content:
+      "cascading 규칙에 따라 다양한 스타일 속성을 활용하여 사용자를 위한 UI 디자인을 할 수 있습니다.",
+  },
+  {
+    title: "React",
+    content:
+      "최근 많이 사용해 가장 좋아하는 frameWork로 컴포넌트로 웹 구조를 구성해 props로 데이터를 전달 받으면서 다양한 훅을 활용해 보고 있습니다. useState, useNavigate, useLocation, useContext 등을 사용한 경험이 있습니다. ",
+  },
+  {
+    title: "Vue",
+    content:
+      "간단한 TodoList와 vuetify로 toy 프로젝트를 만들어 봤습니다. 많이 활용해보지는 않았으나 React와 비슷한 부분이 많았다고 느꼈습니다.  v-directive로 databinding과 컴포넌트의 lifecycle에 따라 호출 될 callBack 함수 등록, route로 페이지 구성을 할 수 있습니다. ",
+  },
+  {
+    title: "Bootstrap",
+    content:
+      "토이 프로젝트로 빠르게 style 주고 싶을 때나 반응형을 할 때 유용하게 사용한 경험이 있습니다 .",
+  },
+  {
+    title: "Tailwind",
+    content:
+      "bootStrap과 비슷한 frameWork로 class를 커스터마이징 하여 재사용에 적절하게 활용할 수 있습니다.",
+  },
+  {
+    title: "Firebase",
+    content:
+      "DB 관리를 위해 사용한 경험이 있습니다. 인증객체와 RealTimeDB를 이용했고, 로그인 기능과 관리자 권한을 설정해 특정 Uid를 관리할 수 있습니다.  ",
+  },
+  {
+    title: "Git",
+    content:
+      "주로 GitBash를 사용하고 리눅스 명령어를 통해 project 생성 및 로컬 저장소에서 원격 저장소로 push, branch를 만들 수 있습니다.",
+  },
+  {
+    title: "GitHub",
+    content:
+      "개발자를 위한 플랫폼 중에 가장 많이 활용하는 플랫폼 입니다. 나를 증명하기 위해 최근 기록 하는 것에 정성을 들이고 있습니다.",
+  },
+  {
+    title: "Figma",
+    content:
+      "컴포넌트를 만들어 활용하고 프로토타입을 통해 시뮬레이션 할 수 있습니다.",
+  },
+  { title: "Photoshop", content: "2023.04.14 GTQ그래픽기술자격 1급 취득" },
+  {
+    title: "Netlify",
+    content: "Netlify로 react 프로젝트 build 및 배포 경험이 있습니다.",
+  },
+];
+
+export default SkillData;
